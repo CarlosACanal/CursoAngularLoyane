b10e3a5933f67c25baaea94d99fe8954913c2257
Carlos Canal <carlos_canal04@hotmail.com>
2023-04-03 21:06:47 -0300

    formularios reativos

--- Formularios/src/app/app.module.ts
+++ Formularios/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DataFormModule } from './data-form/data-form.module';
 import { TemplateFormModule } from './template-form/template-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,14 +6,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { DataFormComponent } from './data-form/data-form.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent,
 
   ],
   imports: [
@@ -20,7 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     TooltipModule.forRoot(),
     FormsModule,
+    ReactiveFormsModule,
     TemplateFormModule,
+    DataFormModule,
     HttpClientModule
   ],
   providers: [],
--- Formularios/src/app/data-form/data-form.component.ts
+++ Formularios/src/app/data-form/data-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -6,5 +8,113 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-form.component.scss']
 })
 export class DataFormComponent {
+
+  form!: FormGroup;
+  form2!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.form2 = this.fb.group({
+      idade: [null],
+      sexo: [null]
+    })
+  }
+
+  ngOnInit() {
+    // this.form = new FormGroup({
+    //   nome : new FormControl('Valor inicial caso queira'),
+    //   email : new FormControl(null),
+    // });
+
+    // Então aqui falamos que esse formulário e um novo objeto da classe FormGroup. Esse novo objeto deve receber como parametro os campos que existirão no forms. Cada um desses campos são um objeto da classe FormControl(). Como parametro podemos passar um valor inicial, por exemplo. Essa é uma forma mais extensa de criarmos o FormGroup
+
+    // O formBuilder nos ajuda a criar o formulário de uma maneira mais rápida e menos verbosa. o fb.group cria um formGroup
+
+    // FORMS PRINCIPAL
+    this.form = this.fb.group({
+      nome: ['Valor inicial caso queira', Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      // FORM DE ENDEREÇO DENTRO DO FORM PRINCIPAL
+      endereco: this.fb.group({
+        cep: [null, Validators.required],
+        numero: [null, Validators.required],
+        complemento: [null],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required]
+      }),
+    })
+  }
+
+  consultaCEP() {
+    let cep = this.form.get('endereco.cep')?.value;
+
+    this.http.get(`http://viacep.com.br/ws/${cep}/json`)
+      .subscribe(r => {
+        console.log(r)
+        this.populaDadosForm(r);
+      });
+
+  }
+
+  populaDadosForm(r: any) {
+    this.form.patchValue({
+      endereco: {
+        bairro: r.bairro,
+        cidade: r.localidade,
+        estado: r.uf
+      }
+    })
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.http.post('https://httpbin.org/post', JSON.stringify(this.form.value)).subscribe(
+        {
+          next: data => {
+            console.log(data);
+            this.resetForm();
+          },
+          error: (error: any) => {
+            alert("Um erro inesperado acorreu!")
+          }
+        }
+      )
+    } else {
+      this.verificaValidacoesForm(this.form)
+    }
+  }
+
+
+  verificaValidacoesForm(formGroup: FormGroup) {
+    // Esse Object.keys consegue ler todas as propiedades de um objeto
+    // Dentro das keys eu to falando pra ler como propiedades todos os controls do form
+    Object.keys(formGroup.controls).forEach(campo => {
+      console.log(campo);
+      const controller = formGroup.get(campo);
+
+      // verifica se o controler é um FormGroup, pq caso seja ele vai rodar a validação novamente para cada controler presente no FormGroup
+      if (controller instanceof FormGroup) {
+        this.verificaValidacoesForm(controller);
+      }
+      if (controller?.invalid) {
+        controller.markAsTouched()
+      }
+    })
+  }
+
+
+  resetForm() {
+    this.form.reset();
+  }
+
+  cssError(control: any) {
+    if (!this.form.get(control)?.valid && this.form.get(control)?.touched) {
+      return true;
+    }
+    return false;
+  }
 
 }
